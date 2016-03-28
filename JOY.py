979363b6fd0be39ee4e88f5107ea493703ce3051
milanlajda <milanlajda1@gmail.com>
2016-03-28 14:18:32 +0100

    added training datasets

--- JOY.py
+++ JOY.py
@@ -1,6 +1,22 @@
+import numpy as np
+
+
+# sigmoid function
+def nonlin(x,deriv=False):
+        if(deriv==True):
+            return x*(1-x)
+        return 1/(1+np.exp(-x))
+
+# input dataset
+x = np.array([0, 0.2, 0.8, 1.4])
+
+# output dataset
+y = np.array([1, 1, 1, 1, ])
+
+
 def joy(annoutput):
     if annoutput == 1:
-        print("It's JOY!")
+        print("TEST: It's JOY!")
 
     else:
-        print("It's not JOY")
+        print("TEST: It's not JOY")
