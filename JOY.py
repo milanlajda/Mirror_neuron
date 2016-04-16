756d81ce40fc48a11abf92c5dc8cea31972e757c
milanlajda <milanlajda1@gmail.com>
2016-04-16 20:08:44 +0100

    Added another layer but I think the problem is that I don't have enough training data or the system recognises some patterns i don't see.

--- JOY.py
+++ JOY.py
@@ -11,8 +11,8 @@ def nonlin(x,deriv=False):
         return 1/(1+np.exp(-x))
 
 # input dataset
-x = np.array([ [0, 1, 0, 1, 0, 1],
-               [1, 0, 1, 0, 1, 0],
+x = np.array([ [0, 1, 1, 0, 0, 1],
+               [1, 0, 0, 0, 1, 0],
                [0, 0, 0, 0, 0, 0],
                [1, 1, 1, 1, 1, 1]])
 
@@ -27,23 +27,32 @@ np.random.seed(1)
 
 # initialize weights randomly with mean 0
 syn0 = 2*np.random.random((6, 4)) - 1
-syn1 = 2*np.random.random((4, 1)) - 1
+syn1 = 2*np.random.random((4, 4)) - 1
+syn2 = 2*np.random.random((4, 1)) - 1
 
 
 # ################################ layer1
-for j in range(10000):
+for j in range(60000):
 
     # forward propagation 1 (ANN guesses the answers)
     l0 = x
     l1 = nonlin(np.dot(l0, syn0))
     l2 = nonlin(np.dot(l1, syn1))
+    l3 = nonlin(np.dot(l2, syn2))
 
     # error calculation
-    l2_error = y - l2
+    l3_error = y - l3
 
     # if (j% 10000) == 0:
     #     print("Error:" + str(np.mean(np.abs(l2_error))))
 
+
+# in what direction is the target value?
+# were we really sure? if so, don't change too much.
+    l3_delta = l3_error*nonlin(l3, deriv=True)
+
+# how much did each l1 value contribute to l2 error (according to the weights)?
+    l2_error = l3_delta.dot(syn2.T)
 
 # in what direction is the target value?
 # were we really sure? if so, don't change too much.
@@ -54,26 +63,32 @@ for j in range(10000):
 
 # in what direction is the target l1?
 # were we really sure? if so don't change much
+    l2_delta = l2_error * nonlin(l2, deriv=True)
+
+# in what direction is the target l1?
+# were we really sure? if so don't change much
     l1_delta = l1_error*nonlin(l1, deriv=True)
 
 
 # update weights
     syn0 += l0.T.dot(l1_delta)
     syn1 += l1.T.dot(l2_delta)
+    syn2 += l2.T.dot(l3_delta)
 
 print("Output of l2 in JOY after training: ")
-print(l2)
+print(l3)
 
 
 # test the ANN with nontraining data
-nontrainingdata1 = np.array([ [0, 1, 0, 1, 0, 0],
-                              [0, 1, 1, 1, 0, 1],
+nontrainingdata1 = np.array([ [1, 1, 1, 0, 0, 0],
+                              [0, 1, 1, 0, 0, 1],
                               [1, 1, 0, 1, 1, 1],
-                              [1, 0, 1, 0, 1, 0]])
+                              [0, 0, 1, 0, 1, 0]])
 test1s1 = nonlin(np.dot(nontrainingdata1, syn0))
 test1s2 = nonlin(np.dot(test1s1, syn1))
+test1s3 = nonlin(np.dot(test1s2, syn2))
 print("Output of JOY with JOY data: ")
-print(test1s2)
+print(test1s3)
 
 
 
